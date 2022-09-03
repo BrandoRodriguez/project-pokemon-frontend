eff2ac32ab31b7e5e657a56d0a9a057118766b5f
Brando Rodriguez <brandorodriguez@gmail.com>
2022-09-03 07:58:47 -0500

    Create Project

--- pages/index.js
+++ pages/index.js
@@ -20,14 +20,14 @@ export default function Home() {
             <div className={styles.nav}>
               <span className={styles.ctn_logo}>
                 <Image
-                  src="/images/logo.svg"
-                  alt="@brandorodriguez"
+                  src='/images/logo.svg'
+                  alt='@brandorodriguez'
                   width={140}
                   height={51}
                 />
               </span>
               <span className={styles.btn1}>
-                <Image src="/images/github.svg" alt="@brandorodriguez" width={28} height={28} />
+                <Image src='/images/github.svg' alt='@brandorodriguez' width={28} height={28} />
               </span>
             </div>
           </>
